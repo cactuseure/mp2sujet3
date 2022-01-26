23da6befa8922f64bbc5655fab3f85c65c7991c0
Mattéo Groult <mattgroult10@gmail.com>
2022-01-26 14:59:14 +0100

    v2

---
+++ formations.php
@@ -0,0 +1,49 @@
+
+<head>
+    <title>Formation l'ULHN</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <a href="formations.php">Formation</a>
+    <a href="#">Litis</a>
+    <a href="#">Stage</a>
+    <a href="#">Profil</a>
+</div>
+
+<div id="main">
+    <div class="navbar">
+        <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Menu</span>
+
+    </div>
+    <div class="cards">
+        <div class="card">ONE</div>
+        <div class="card">TWO</div>
+        <div class="card">THREE</div>
+        <div class="card">FOUR</div>
+        <div class="card">FIVE</div>
+        <div class="card">SIX</div>
+        <div class="card">SEVEN</div>
+        <div class="card">EIGHT</div>
+        <div class="card">NINE</div>
+        <div class="card">TEN</div>
+        <div class="card">ELEVEN</div>
+        <div class="card">TWELVE</div>
+    </div>
+</div>
+
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+</script>
+   
